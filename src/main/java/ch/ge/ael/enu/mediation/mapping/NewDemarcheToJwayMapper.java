6bfe93e0897d7a7a0d3bac687e89ad35f3dee175
Yves Dubois-Pèlerin <yves.dubois-pelerin@etat.ge.ch>
2020-11-30 17:15:56 +0100

    ENU-427 Changement d'état d'une demande (suite)

--- src/main/java/ch/ge/ael/enu/mediation/mapping/NewDemarcheToJwayMapper.java
+++ src/main/java/ch/ge/ael/enu/mediation/mapping/NewDemarcheToJwayMapper.java
@@ -1,34 +1,60 @@
 package ch.ge.ael.enu.mediation.mapping;
 
 import ch.ge.ael.enu.mediation.jway.model.*;
+import ch.ge.ael.enu.mediation.metier.model.DemarcheStatus;
 import ch.ge.ael.enu.mediation.metier.model.NewDemarche;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import static ch.ge.ael.enu.mediation.metier.model.DemarcheStatus.BROUILLON;
+import static ch.ge.ael.enu.mediation.metier.model.DemarcheStatus.SOUMISE;
+import static ch.ge.ael.enu.mediation.metier.model.DemarcheStatus.EN_COURS;
+
 @Configuration
 public class NewDemarcheToJwayMapper {
 
     public File newDemarcheToFile(NewDemarche newDemarche) {
         File file = new File();
+
         file.setName(newDemarche.getIdClientDemande());
+
         User owner = new User();
         owner.setName(newDemarche.getIdUsager());
         file.setOwner(owner);
+
         Application application = new Application();
         application.setName(newDemarche.getIdPrestation());
         file.setApplication(application);
+
         file.setWorkflowStatus(new StatusMapper().mapStringToJway((newDemarche.getEtat())));
+
         file.setStatus(new StatusMapper().mapStringToJway((newDemarche.getEtat())));
-        Form form = new Form();
-        form.setNameLabel(new Text("Label", new HashMap<>() {{
-            put("fr", newDemarche.getLibelleAction());
-        }}));
-        form.setUrls(new ArrayList<>() {{
-            add(new FormUrl(newDemarche.getUrlAction()));
-        }});
+
+        if (is(newDemarche, BROUILLON) && newDemarche.getUrlAction() != null) {
+            StringBuilder sb = new StringBuilder()
+                    .append("|")
+                    .append(newDemarche.getLibelleAction())
+                    .append("|")
+                    .append(newDemarche.getUrlAction());
+            file.setStepDescription(sb.toString());
+//            file.setToDate(newDemarche.getDateEcheanceAction().atStartOfDay());  // cree une HTTP 400
+        }
+        /*
+        else if (is(newDemarche, SOUMISE)) {
+            file.setFromDate(newDemarche.getDateDepot());
+        } else if (is(newDemarche, EN_COURS)) {
+            file.setFromDate(newDemarche.getDateDepot());
+            file.setToDate(newDemarche.getDateMiseEnTraitement());
+        }
+         */
+
         return file;
+    }
+
+    private boolean is(NewDemarche newDemarche, DemarcheStatus status) {
+        return DemarcheStatus.valueOf(newDemarche.getEtat()) == status;
     }
 
 }
---
+++ src/main/java/ch/ge/ael/enu/mediation/serialization/JwayDateDeserializer.java
@@ -0,0 +1,32 @@
+package ch.ge.ael.enu.mediation.serialization;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+/**
+ * Transforme en LocalDateTime une date reçue de FormServices, comme "2020-11-25T15:42:05.445+0000".
+ */
+public class JwayDateDeserializer extends StdDeserializer<LocalDateTime> {
+
+    private static final DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS+0000", Locale.FRENCH);
+
+    public JwayDateDeserializer() {
+        this(null);
+    }
+
+    public JwayDateDeserializer(Class<LocalDateTime> t) {
+        super(t);
+    }
+
+    @Override
+    public LocalDateTime deserialize(JsonParser parser, DeserializationContext ctx) throws IOException {
+        return LocalDateTime.parse(parser.getText().toString(), FORMAT);
+    }
+
+}
