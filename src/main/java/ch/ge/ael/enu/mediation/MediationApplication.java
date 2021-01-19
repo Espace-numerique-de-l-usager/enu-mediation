a6e3c3bb4077420aa1ad9b203a224154873d50a7
Yves Dubois-Pèlerin <yves.dubois-pelerin@etat.ge.ch>
2021-01-19 16:28:53 +0100

    Configuration des traces pour Tomcat

--- src/main/java/ch/ge/ael/enu/mediation/MediationApplication.java
+++ src/main/java/ch/ge/ael/enu/mediation/MediationApplication.java
@@ -2,9 +2,10 @@ package ch.ge.ael.enu.mediation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MediationApplication {
+public class MediationApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MediationApplication.class, args);
