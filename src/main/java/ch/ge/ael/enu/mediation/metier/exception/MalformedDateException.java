490bee79ce20740a295713919b16ed61383d3e32
Yves Dubois-Pèlerin <yves.dubois-pelerin@etat.ge.ch>
2021-01-21 18:58:48 +0100

    Blindage des contrats

--- src/main/java/ch/ge/ael/enu/mediation/metier/exception/MalformedDateException.java
+++ src/main/java/ch/ge/ael/enu/mediation/metier/exception/MalformedDateException.java
@@ -7,7 +7,7 @@ package ch.ge.ael.enu.mediation.metier.exception;
 public class MalformedDateException extends ValidationException {
 
     public MalformedDateException(String value, String fieldName) {
-        super("La valeur \"" + value + "\" du champ \"" + fieldName + "\" est incorrecte. Le format attendu est \"yyyy-MM-dd\".");
+        super("La valeur \"" + value + "\" du champ \"" + fieldName + "\" est incorrecte. Le format attendu est \"yyyy-MM-dd\"");
     }
 
 }
