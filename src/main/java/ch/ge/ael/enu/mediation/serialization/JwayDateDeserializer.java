c7e46c7a05f4dd04302649de97b69bdc91f0a618
Yves Dubois-Pèlerin <yves.dubois-pelerin@etat.ge.ch>
2021-02-03 17:25:47 +0100

    Corrections diverses : URL, failles SonarQube, désérialisation dates

--- src/main/java/ch/ge/ael/enu/mediation/serialization/JwayDateDeserializer.java
+++ src/main/java/ch/ge/ael/enu/mediation/serialization/JwayDateDeserializer.java
@@ -10,11 +10,14 @@ import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 /**
- * Transforme en LocalDateTime une date reçue de FormServices, comme "2020-11-25T15:42:05.445+0000".
+ * Transforme en LocalDateTime une date reçue de FormServices, comme "2020-11-25T15:42:05.445+0000" ou
+ * "2020-11-25T15:42:05.445+00:00".
  */
 public class JwayDateDeserializer extends StdDeserializer<LocalDateTime> {
 
-    private static final DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS+0000", Locale.FRENCH);
+    private static final DateTimeFormatter FORMAT_1 = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS+0000", Locale.FRENCH);
+
+    private static final DateTimeFormatter FORMAT_2 = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS+00:00", Locale.FRENCH);
 
     public JwayDateDeserializer() {
         this(null);
@@ -26,7 +29,12 @@ public class JwayDateDeserializer extends StdDeserializer<LocalDateTime> {
 
     @Override
     public LocalDateTime deserialize(JsonParser parser, DeserializationContext ctx) throws IOException {
-        return LocalDateTime.parse(parser.getText(), FORMAT);
+        // on essaie avec un format, puis avec l'autre
+        try {
+          return LocalDateTime.parse(parser.getText(), FORMAT_1);
+        } catch (Exception e) {
+          return LocalDateTime.parse(parser.getText(), FORMAT_2);
+        }
     }
 
 }
