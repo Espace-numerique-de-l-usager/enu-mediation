09904cdab8c738abb0910f21478ed1c5fb9b3139
Mathe Guillaume (DI) <guillaume.mathe@etat.ge.ch>
2021-03-04 14:21:44 +0100

    Config nettoyée pour dev+rec+prod

--- src/main/java/ch/ge/ael/enu/mediation/MediationApplication.java
+++ src/main/java/ch/ge/ael/enu/mediation/MediationApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = "classpath:enu-mediation-default.properties")  // pour execution locale et pour execution sous Tomcat
 @PropertySource(value = "file:${catalina.base}/conf/enu-mediation.properties", ignoreResourceNotFound = true)  // uniquement pour execution sous Tomcat
-//@PropertySource(value = "classpath:enu-mediation-default.properties")  // pour execution locale et pour execution sous Tomcat
 public class MediationApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
