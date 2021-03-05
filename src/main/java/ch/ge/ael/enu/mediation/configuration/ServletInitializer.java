3d26b63d8cdfe3b123141c99a54ff0ca1fbfc1bd
Mathe Guillaume (DI) <guillaume.mathe@etat.ge.ch>
2021-03-05 13:55:57 +0100

    Fix config médiation pour dev local + déploiement dans les environnements

---
+++ src/main/java/ch/ge/ael/enu/mediation/configuration/ServletInitializer.java
@@ -0,0 +1,19 @@
+package ch.ge.ael.enu.mediation.configuration;
+
+import ch.ge.ael.enu.mediation.MediationApplication;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@Slf4j
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    private final String tomcatHome = System.getProperty("catalina.home");
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MediationApplication.class)
+                .properties("spring.config.name: application,enu-mediation")
+                .properties("spring.config.location: " + tomcatHome + "/conf/,classpath:/");
+    }
+}
