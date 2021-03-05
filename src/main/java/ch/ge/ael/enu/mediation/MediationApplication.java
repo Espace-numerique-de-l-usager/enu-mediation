f341fa093634f9c5339bcf78e3a61f895abbd388
Mathe Guillaume (DI) <guillaume.mathe@etat.ge.ch>
2021-03-05 13:55:57 +0100

    Fix config médiation pour dev local + déploiement dans les environnements

--- src/main/java/ch/ge/ael/enu/mediation/MediationApplication.java
+++ src/main/java/ch/ge/ael/enu/mediation/MediationApplication.java
@@ -3,15 +3,12 @@ package ch.ge.ael.enu.mediation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource(value = "classpath:enu-mediation-default.properties")  // pour execution locale et pour execution sous Tomcat
-@PropertySource(value = "file:***REMOVED***/conf/enu-mediation.properties")  // uniquement pour execution sous Tomcat
 public class MediationApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
+        System.setProperty("spring.config.name", "application,enu-mediation");
         SpringApplication.run(MediationApplication.class, args);
     }
-
 }
