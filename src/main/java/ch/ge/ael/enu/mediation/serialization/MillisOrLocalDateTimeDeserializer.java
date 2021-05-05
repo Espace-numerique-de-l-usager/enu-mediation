2a3a57c3e8add42d04630871ba659e6b7b9aeba3
Yves Dubois-Pèlerin <yves.dubois-pelerin@etat.ge.ch>
2021-05-05 14:59:01 +0200

    Déplacement d'une classe

--- src/main/java/ch/ge/ael/enu/mediation/serialization/MillisOrLocalDateTimeDeserializer.java
+++ src/main/java/ch/ge/ael/enu/mediation/serialization/MillisOrLocalDateTimeDeserializer.java
@@ -1,4 +1,4 @@
-package ch.ge.ael.enu.mediation.configuration;
+package ch.ge.ael.enu.mediation.serialization;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
@@ -14,7 +14,7 @@ import java.time.format.DateTimeFormatter;
 /**
  * Deserialisation automatique des timestamps "raw" vers java.time.LocalDateTime
  */
-class MillisOrLocalDateTimeDeserializer extends LocalDateTimeDeserializer {
+public class MillisOrLocalDateTimeDeserializer extends LocalDateTimeDeserializer {
 
     public MillisOrLocalDateTimeDeserializer() {
         super(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
